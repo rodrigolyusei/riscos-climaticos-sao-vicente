c18ceefe6bac48257362e7b350f326c5d703c8e3
Rodrigo Lyusei <rodrigo.lyusei@gmail.com>
2026-07-26 14:50:07 -0300

    feat: back to openstreet

--- src/map/config.ts
+++ src/map/config.ts
@@ -19,23 +19,23 @@ const TILE_PROVIDERS: Record<string, { url: string; attribution: string }> = {
     attribution:
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   },
-  EsriWorldTopoMap: {
-    url: "https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}",
-    attribution:
-      "Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community",
-  },
   CartoDB: {
     url: "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png",
     attribution:
       '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attributions">CARTO</a>',
   },
+  EsriWorldTopoMap: {
+    url: "https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}",
+    attribution:
+      "Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community",
+  },
 };
 
-export const TILE_URL = TILE_PROVIDERS.EsriWorldTopoMap.url;
+export const TILE_URL = TILE_PROVIDERS.OpenStreetMap.url;
 export const TILE_OPTIONS = {
   maxZoom: 19,
   buffer: 3,
   keepBuffer: 8,
   updateWhenIdle: false,
-  attribution: TILE_PROVIDERS.EsriWorldTopoMap.attribution,
+  attribution: TILE_PROVIDERS.OpenStreetMap.attribution,
 };
